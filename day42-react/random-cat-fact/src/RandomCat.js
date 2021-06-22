a21ade32d7d9f4a1341154448c6d2feb1eb41747
Slavo Kozar <slavo.kozar@gmail.com>
2021-06-22 15:49:07 +0200

    random cat fact demo

---
+++ day42-react/random-cat-fact/src/RandomCat.js
@@ -0,0 +1,37 @@
+import {useState, useEffect} from "react";
+
+function RandomCat(){
+    const [catFact, setCatFact] = useState(null);
+
+    async function fetchData(){
+        const response = await fetch('https://cat-fact.herokuapp.com/facts/random');
+
+        const data = await response.json();
+
+        setCatFact(data);
+    }
+
+    useEffect(() => {
+        console.log('i am going to fetch data')
+        fetchData();
+
+
+        setInterval(() => {
+            fetchData()
+        }, 2000)
+
+
+    }, [])
+
+    if(catFact === null) return null;
+
+    return (
+        <div>
+        <h1>{catFact.text}</h1>
+
+            <button onClick={() => { fetchData() }}>Fetch!</button>
+        </div>
+    )
+}
+
+export default RandomCat;
